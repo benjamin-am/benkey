c9a622a23f2756d884fd0c56cce8ecfd9dd27b2d
Benjamin LeBlanc <benleb@students.cs.ubc.ca>
2025-02-12 14:21:56 -0800

    Initial specification for Password class

---
+++ src/main/model/Password.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Password {
+    
+    /* 
+    REQUIRES: password length is greater than 0, encrypted password > 0;
+    EFFECTS: sets up a new password and encrypted password
+    */
+    public Password(String password) {
+
+    }
+
+    public void setPassword() {
+
+    }
+
+    public String getPassword() {
+        return "";
+    }
+
+    /* 
+    EFFECTS: creates an encrypted password
+    */
+    public String encryptPassword() {
+        return "";
+    }
+
+    /* 
+    EFFECTS: decrypts an encrypted password
+    */
+    public String decryptPassword() {
+        return "";
+    }
+
+}
