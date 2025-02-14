a781f9eb895d70f5d02a34654f285be7ede58350
Benjamin LeBlanc <benleb@students.cs.ubc.ca>
2025-02-13 22:25:19 -0800

    Initial implementation of Password Class, not using hash algorithm for encryption yet

--- src/main/model/Password.java
+++ src/main/model/Password.java
@@ -1,39 +1,40 @@
 package model;
 
 public class Password {
-    
+    private String encryptedPassword;
     /* 
     REQUIRES: password length is greater than 0, encrypted password > 0;
     EFFECTS: sets up a new password and encrypted password
     */
     public Password(String password) {
-
+        this.encryptedPassword = encryptPassword(password);
     }
 
     public void setPassword(String pass) {
-
+        this.encryptedPassword = encryptPassword(pass);
     }
 
     public String getPassword() {
-        return "";
+        return decryptPassword();
     }
 
     public String getEncryptPassword() {
-        return "";
+        return this.encryptedPassword;
     }
 
     /* 
     EFFECTS: creates an encrypted password
     */
-    private String encryptPassword() {
-        return "";
+    private String encryptPassword(String password) {
+        return password + "C1";
     }
 
     /* 
     EFFECTS: decrypts an encrypted password
     */
     private String decryptPassword() {
-        return "";
+        int size = encryptedPassword.length();
+        return encryptedPassword.substring(0, size - 2);
     }
 
 }
