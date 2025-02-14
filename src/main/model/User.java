13327a8076375a0d3b6cd5f34ec8b18770ff69e6
Benjamin LeBlanc <benleb@students.cs.ubc.ca>
2025-02-14 00:39:33 -0800

    Initial implementation of User class

--- src/main/model/User.java
+++ src/main/model/User.java
@@ -2,9 +2,13 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 // A user of the password vault. Has a login (username/password) and has a list of accounts
 public class User {
+    private String username;
+    private Password password;
+    private List<Account> accounts;
 
     /* 
     REQUIRES: a user's username and password must be stringlength > 0
@@ -12,39 +16,41 @@ public class User {
             profile with an empty list. The user MUST remember their password.
     */
     public User(String username, String password) {
-        //stub
+        this.username = username;
+        this.password = new Password(password);
+        this.accounts = new ArrayList<>();
     }
 
     // getters
     public String getUsername() {
-        return null; // stub
+        return this.username; // stub
     }
 
     public Password getPassword() {
-        return null; // stub
+        return this.password; // stub
     }
 
     public List<Account> getListOfAccounts() {
-        return null; // stub
+        return this.accounts; // stub
     }
     // setters
-    public void setUsername() {
-        // stub
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public void setPassword() {
-
+    public void setPassword(String pass) {
+        this.password = new Password(pass);
     }
 
-    public void setAccountList() {
-
+    public void setAccountList(List<Account> accounts) {
+        this.accounts = accounts;
     }
     /* 
     MODIFIES: this
     EFFECTS: adds account to the user's profile
     */
     public void addAccount(Account account) {
-        //stub
+        this.accounts.add(account);
     }
 
     /* 
@@ -52,42 +58,49 @@ public class User {
     EFFECTS: removes account from the user's profile
     */
     public void removeAccount(Account account) {
-        //stub
+        this.accounts.remove(account);
     }
 
     /* 
     EFFECTS: returns number of accounts user has
     */
     public int totalAccounts() {
-        return 1;
+        return accounts.size();
     }
 
     /* 
     EFFECTS: returns number of Websites a user has an account for
     */
     public int totalWebsites() {
-        return 1;
+       return listAllWebsites().size();
     }
 
     /* 
     EFFECTS: returns list of distinct websites user has an account for
     */
     public List<Website> listAllWebsites() {
-        return null; //stub
+        List<Website> websites = new ArrayList<>();
+        for (Account acc : accounts) {
+            Website web = acc.getWebsite();
+            if (!websites.contains(web)) {
+                websites.add(web);
+            }
+        }
+        return websites;
     }
 
     /* 
     EFFECTS: returns list of distinct usernames user in accounts
     */
     public List<String> listAllUsernames() {
-        return null; //stub
+        return accounts.stream().map(a -> a.getUsername()).distinct().collect(Collectors.toList()); //stub
     }
 
     /* 
     EFFECTS: returns true if password matches
     */
     public boolean verifyPassword(String pass) {
-        return false;
+        return password.getPassword().equals(pass);
     }
 
 }
