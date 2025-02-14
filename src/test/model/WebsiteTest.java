38630dbd834417bf7117d83381b07b419f4b042b
Benjamin LeBlanc <benleb@students.cs.ubc.ca>
2025-02-13 17:11:31 -0800

    Added initial Website class tests

---
+++ src/test/model/WebsiteTest.java
@@ -0,0 +1,51 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class WebsiteTest {
+    private Website w1;
+
+    @BeforeEach
+    void runBefore() {
+        w1 = new Website("Test", "test.com");
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("Test", w1.getName());
+        assertEquals("test.com", w1.getUrl());
+    }
+
+    @Test
+    void testNameSetter() {
+        w1.setName("anotherName");
+        assertEquals("anotherName", w1.getName());
+        w1.setName("anotherName");
+        w1.setName("anotherName");
+        assertEquals("anotherName", w1.getName());
+        
+        w1.setName("anotherName");
+        w1.setName("a");
+        assertEquals("a", w1.getName());
+    }
+
+    @Test
+    void testUrlSetter() {
+        w1.setUrl("another.com");
+        assertEquals("another.com", w1.getUrl());
+        w1.setUrl("another.com");
+        w1.setUrl("another.com");
+        assertEquals("another.com", w1.getUrl());
+        
+        w1.setUrl("anotherName");
+        w1.setUrl("a");
+        assertEquals("a", w1.getUrl());
+    }
+}
+
