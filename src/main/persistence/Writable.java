825bbce6695dbae9fe88457253ecdab185861df5
Benjamin LeBlanc <benleb@students.cs.ubc.ca>
2025-02-25 23:49:25 -0800

    Initialize and specify interface for Writeable class

---
+++ src/main/persistence/Writable.java
@@ -0,0 +1,10 @@
+package persistence;
+// Based on the Writable class in the JsonSerializationDemo
+
+import org.json.JSONObject;
+
+public interface Writable {
+    // EFFECTS: returns this object as a JSON object
+    JSONObject toJson();
+
+}
