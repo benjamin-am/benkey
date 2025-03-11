0ce72d06300cbba9cb43b7cda0e7b16348a1a3a8
Benjamin LeBlanc <benleb@students.cs.ubc.ca>
2025-03-11 13:05:42 -0700

    Updated main method to chose UI or GUI

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,19 @@
 package ui;
 
+import java.util.Scanner;
+
+import ui.gui.PasswordVaultGUI;
+
 public class Main {
+    private static Scanner scan;
     public static void main(String[] args) throws Exception {
-        new PasswordVault();
+        scan = new Scanner(System.in);
+        System.out.println("Launch UI or GUI? (answer U/G for respective interface)");
+        String input = scan.next().toLowerCase();
+        if (input.equals("u")) {
+            new PasswordVault();
+        } else {
+            new PasswordVaultGUI();
+        }
     }
 }
