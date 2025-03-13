e53b955f6bf81ed97248acf264bd8244eaccb087
Benjamin LeBlanc <benleb@students.cs.ubc.ca>
2025-03-13 13:20:04 -0700

    New defaults aadded such as background color, fontcolor, and headerfont

--- src/main/ui/gui/Defaults.java
+++ src/main/ui/gui/Defaults.java
@@ -1,17 +1,23 @@
 package ui.gui;
 
 import java.awt.Font;
+import java.awt.Color;
 
 // Attempt at using Singleton based on https://refactoring.guru/design-patterns/singleton
 public class Defaults {
     private final Font font;
     private static Defaults defaults;
+    private Color background;
+    private Color fontColor;
+    private Font headerFont;
 
 
     // EFFECTS: initialize Defaults
     private Defaults() {
         font = new Font("Lucida Sans", Font.PLAIN, 18);
-
+        headerFont = new Font("Lucida Sans", Font.BOLD, 30);
+        background = Color.decode("#2D2D2D");
+        fontColor = Color.decode("#E1E1E1");
     }
 
     // EFFECTS: returns Defaults object
@@ -26,5 +32,20 @@ public class Defaults {
     // EFFECTS: returns font
     public Font getFont() {
         return font;
+    }
+
+    // EFFECTS: returns font
+    public Font getHeaderFont() {
+        return headerFont;
+    }
+
+    // EFFECTS: returns font
+    public Color getFontColor() {
+        return fontColor;
+    }
+
+    // EFFECTS: returns font
+    public Color getBackgroundColor() {
+        return background;
     }
 }
