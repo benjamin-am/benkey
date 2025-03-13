6fe626f1c892279a1e0776b7fede22ae67c1aafc
Benjamin LeBlanc <benleb@students.cs.ubc.ca>
2025-03-13 13:21:36 -0700

    Initial account panel

--- src/main/ui/gui/AccountPanel.java
+++ src/main/ui/gui/AccountPanel.java
@@ -3,17 +3,19 @@ package ui.gui;
 import java.awt.Color;
 import java.awt.FlowLayout;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 
 public class AccountPanel extends JPanel {
     private Defaults defaults = Defaults.getDefaults();
     private PasswordVaultGUI passVault;
+    private JButton backButton;
+
 
     public AccountPanel(PasswordVaultGUI passVault) {
         this.passVault = passVault;
-        this.setBackground(Color.red);
+        this.setBackground(defaults.getBackgroundColor());
         this.setBounds(0, 0, 250, 250);
         
         JLabel label = new JLabel();
@@ -22,6 +24,14 @@ public class AccountPanel extends JPanel {
 
         this.setLayout(new FlowLayout());
         this.add(label);
+        backButtonInit();
+        this.add(backButton);
+    }
+
+    // EFFECTS: sets up back button
+    private void backButtonInit() {
+        backButton = ButtonFactory.createButton("back", 200, 100, 100, 50);
+        backButton.addActionListener(e -> passVault.changeScreen(PasswordVaultGUI.getIntro()));
     }
 
 }
