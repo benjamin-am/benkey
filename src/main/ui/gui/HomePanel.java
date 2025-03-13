241c34351747fc1dd432f8fe2e5debfb072a3097
Benjamin LeBlanc <benleb@students.cs.ubc.ca>
2025-03-13 13:21:12 -0700

    Initial homepanel

---
+++ src/main/ui/gui/HomePanel.java
@@ -0,0 +1,14 @@
+package ui.gui;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class HomePanel extends JPanel {
+    PasswordVaultGUI passVault;
+
+    public HomePanel(PasswordVaultGUI passVault) {
+        this.passVault = passVault;
+    }
+}
