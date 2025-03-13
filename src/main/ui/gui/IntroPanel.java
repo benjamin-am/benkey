5b4c1b38789271c585fbb4e8ce82e0fbb675ad3b
Benjamin LeBlanc <benleb@students.cs.ubc.ca>
2025-03-13 13:20:45 -0700

    Uses newly updated factory methods and defaults

--- src/main/ui/gui/IntroPanel.java
+++ src/main/ui/gui/IntroPanel.java
@@ -3,6 +3,7 @@ package ui.gui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -18,16 +19,14 @@ public class IntroPanel extends JPanel {
 
     public IntroPanel(PasswordVaultGUI passVault) {
         this.passVault = passVault;
-        this.setBackground(Color.red);
+        this.setBackground(defaults.getBackgroundColor());
         this.setBounds(0, 0, 250, 250);
         
-        JLabel label = new JLabel();
-        label.setText("Welcome to benkey. Please login or create a new account.");
-        label.setFont(defaults.getFont());
-
+        JLabel label = LabelFactory.createLabel("<html>Welcome to benkey.<br/> Please login or create a new account.</html>");
+        
         buttonInit();
 
-        this.setLayout(new FlowLayout());
+        this.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 20));
         this.add(label);
         this.add(accountButton);
         this.add(loginButton);
@@ -40,18 +39,14 @@ public class IntroPanel extends JPanel {
 
     // EFFECTS: sets up new account button
     private void newAccountButtonInit() {
-        accountButton = new JButton("New Account");
-        accountButton.setBounds(200, 200, 100, 50);
+        accountButton = ButtonFactory.createButton("New Account", 200, 200, 100, 50);
         accountButton.addActionListener(e -> passVault.changeScreen(PasswordVaultGUI.getNewAccount()));
-        accountButton.setFont(defaults.getFont());
     }
 
     // EFFECTS: sets up login button
     private void loginButtonInit() {
-        loginButton = new JButton("Login");
-        loginButton.setBounds(200, 100, 100, 50);
+        loginButton = ButtonFactory.createButton("Login", 200, 100, 100, 50);
         loginButton.addActionListener(e -> passVault.changeScreen(PasswordVaultGUI.getLogin()));
-        loginButton.setFont(defaults.getFont());
     }
     
 
