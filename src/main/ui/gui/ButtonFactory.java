2f2e4db52a6329808853b366714fd3320a2aadf2
Benjamin LeBlanc <benleb@students.cs.ubc.ca>
2025-03-13 18:53:24 -0700

    Added preferred and maximum size functions to ButtonFactory

--- src/main/ui/gui/ButtonFactory.java
+++ src/main/ui/gui/ButtonFactory.java
@@ -1,5 +1,6 @@
 package ui.gui;
 
+import java.awt.Dimension;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
@@ -11,18 +12,32 @@ public class ButtonFactory {
     // EFFECTS: Creates a button to use in a JPanel or JFrame
     public static JButton createButton(String text, int x, int y, int width, int height) {
         JButton button = new JButton(text);
+        setBoundsFontFocus(x, y, width, height, button);
+        return button;
+    }
+
+    // EFFECTS: Creates a button to use in a JPanel or JFrame, with no painted border
+    public static JButton createButton(String text, int x, int y, int width, int height, boolean border) {
+        JButton button = new JButton(text);
+        setBoundsFontFocus(x, y, width, height, button);
+        button.setBorderPainted(border);
+        button.setBackground(defaults.getButtonColor());
+        return button;
+    }
+
+    private static void setBoundsFontFocus(int x, int y, int width, int height, JButton button) {
         button.setBounds(x, y, width, height);
+        button.setPreferredSize(new Dimension(width, height));
+        button.setMaximumSize(new Dimension(width, height));
         button.setFont(defaults.getFont());
         button.setFocusable(false);
-        return button;
+        button.setBackground(defaults.getButtonColor());
     }
 
     // EFFECTS: Creates a button to use in a JPanel or JFrame
     public static JButton createButton(String text, int x, int y, int width, int height, ActionListener al) {
         JButton button = new JButton(text);
-        button.setBounds(x, y, width, height);
-        button.setFont(defaults.getFont());
-        button.setFocusable(false);
+        setBoundsFontFocus(x, y, width, height, button);
         button.addActionListener(al);
         return button;
     }
