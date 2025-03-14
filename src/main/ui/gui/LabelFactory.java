c3cf31b2061f6ce9a46308093d196913746e36ee
Benjamin LeBlanc <benleb@students.cs.ubc.ca>
2025-03-13 18:53:57 -0700

    Added sizing to LabelFactory for max and preferred

--- src/main/ui/gui/LabelFactory.java
+++ src/main/ui/gui/LabelFactory.java
@@ -1,5 +1,6 @@
 package ui.gui;
 
+import java.awt.Dimension;
 import javax.swing.JLabel;
 
 // A factory class for creating labels based on https://refactoring.guru/design-patterns/factory-method
@@ -9,16 +10,22 @@ public class LabelFactory {
     // EFFECTS: Creates a label
     public static JLabel createLabel(String text, int x, int y, int width, int height) {
         JLabel label = new JLabel(text);
-        label.setBounds(x, y, width, height);
+        setSizing(x, y, width, height, label);
         fontAndColor(label);
         return label;
+    }
+
+    private static void setSizing(int x, int y, int width, int height, JLabel label) {
+        label.setBounds(x, y, width, height);
+        label.setPreferredSize(new Dimension(width, height));
+        label.setMaximumSize(new Dimension(width, height));
     }
 
     // EFFECTS: creates a header label
     public static JLabel createLabel(String text, int x, int y, int width, int height, boolean header) {
         if (header) {
             JLabel label = new JLabel(text);
-            label.setBounds(x, y, width, height);
+            setSizing(x, y, width, height, label);
             label.setFont(defaults.getHeaderFont());
             label.setForeground(defaults.getFontColor());
             return label;
