cf94eefc7748d8663477448f9eca7a47f3d5d7c4
Benjamin LeBlanc <benleb@students.cs.ubc.ca>
2025-03-22 19:09:35 -0700

    Created an image handler class to grab images from specified file path

---
+++ src/main/ui/gui/panels/ImageHandler.java
@@ -0,0 +1,23 @@
+package ui.gui.panels;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+// Handles image addition to panels
+public class ImageHandler {
+
+    // Constructor - nothing really needed
+    public ImageHandler() {
+        
+    }
+
+    // EFFECTS: produces an image for the user, with given width and height at the specified file path
+    // ATTRIBUTION: https://stackoverflow.com/questions/299495/how-to-add-an-image-to-a-jpanel
+    public ImageIcon getImage(String filePath, int width, int height) {
+        ImageIcon imageIcon = new ImageIcon(filePath);
+        Image img = imageIcon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        return new ImageIcon(img);
+    }
+
+}
